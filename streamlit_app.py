b948fb69e5f7fbceb2d0c2cfb6f17fb643cd12b1
avinash97422 <136719039+avinash97422@users.noreply.github.com>
2023-06-15 19:36:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit
 
 streamlit.title('my parents new healthy diner')
-
+COPY THESE: 
+            
 streamlit.header('Breakfast favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('avocado toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
