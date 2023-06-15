4be819380cbbd4cc7a5b1494804c8189ad574cf1
avinash97422 <136719039+avinash97422@users.noreply.github.com>
2023-06-15 19:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.header('Breakfast favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('avocado toast')
 
 
